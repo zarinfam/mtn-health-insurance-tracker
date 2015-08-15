09f24b0bdfe0b28c4e33b9bdcae880aadf9c5755
zarinfam.s <zarinfam.s@gmail.com>
2015-08-15 09:08:34 +0430

    Add ClickListener for test button in main activity.

--- app/src/main/java/ir/irancell/course/Insurance/MainActivity.java
+++ app/src/main/java/ir/irancell/course/Insurance/MainActivity.java
@@ -4,14 +4,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView textViewTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewTest = (TextView) findViewById(R.id.textViewTest);
     }
 
     @Override
@@ -34,5 +40,9 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showTest(View view) {
+        textViewTest.setText("this is a test.");
     }
 }
